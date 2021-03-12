e832e049f1460e0eb3f67f687fbe747a5032ad9b
Sharod Duncan <sharod@me.com>
2021-03-12 09:55:25 -0800

    whitespace exercise

---
+++ whitespace.py
@@ -0,0 +1,10 @@
+print("Python")
+
+#this is how you enter a tab whitespace
+print("\tPython")
+
+#this is how you enter a new line whitespace
+print("Languages:\nPython\nC\nJavaScript")
+
+#this is how you combine new line and tab whitespaces
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
