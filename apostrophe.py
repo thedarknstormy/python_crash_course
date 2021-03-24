4c0bafec07db9b64b268931d32a24b757862b4a9
Sharod Duncan <sharod@me.com>
2021-03-24 09:08:45 -0700

    apostrophy exercise

---
+++ apostrophe.py
@@ -0,0 +1,7 @@
+#use double quotation marks "" when using an apostrophe in a string
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+#this is an example of a syntax error in a string. 
+message = 'One of Python's strengths is its diverse community.'
+print(message)
