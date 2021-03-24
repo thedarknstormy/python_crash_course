aad5744f95c6585a86bc83516ffcff023521cb7f
Sharod Duncan <sharod@me.com>
2021-03-24 09:26:09 -0700

    integers exercise

---
+++ integers.py
@@ -0,0 +1,26 @@
+add = 2 + 3
+print(add)
+
+subtract = 3 - 2
+print(subtract)
+
+multiply = 2 * 3
+print(multiply)
+
+divide = 3 / 2
+print(divide)
+
+exponents1 = 3 ** 2
+print(exponents1)
+
+exponents2 = 3 ** 3
+print(exponents2)
+
+exponents3 = 10 ** 6
+print(exponents3)
+
+order_of_operation1 = 2 + 3 * 4
+print(order_of_operation1)
+
+order_of_operation2 = (2 + 3) * 4
+print(order_of_operation2)
