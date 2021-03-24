33a36dec1de2d866d9ae0a18bbbb64e7d853be04
Sharod Duncan <sharod@me.com>
2021-03-24 09:22:51 -0700

    famous quote exercise

---
+++ famous_quote.py
@@ -0,0 +1,8 @@
+quote = "Leap and the net will appear."
+author = "John Burroughs"
+
+#this line will create a variable that combines the strings quote and author into a single string
+quote_and_author = f'{author} once said, {quote}'
+
+#this line will print the variable quote_and_author
+print(quote_and_author)
