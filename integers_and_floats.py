59d05e48206a9df00420e8515e89e1f6f9471944
Sharod Duncan <sharod@me.com>
2021-03-24 09:44:41 -0700

    integers and floats exercise

---
+++ integers_and_floats.py
@@ -0,0 +1,10 @@
+#if you mix an integer and a float in any other operation, you'll get a float as well
+
+integer_and_float1 = 1 + 2.0
+print(integer_and_float1)
+
+integer_and_float2 = 2 * 3.0
+print(integer_and_float2)
+
+integer_and_float3 = 3.0 ** 2
+print(integer_and_float3)
