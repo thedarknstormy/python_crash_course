2746cc81623b694966a983eaa0e537ea5c64f765
Sharod Duncan <sharod@me.com>
2021-03-24 09:14:19 -0700

    personal message exercise

---
+++ personal_message.py
@@ -0,0 +1,5 @@
+name = "sharod"
+
+#.title() is a method that capitalizes the first letter of the string
+message = f"Hello {name.title()}, would you like to learn some Python today?"
+print(message)
