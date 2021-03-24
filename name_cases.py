5b50c722a813d9aa096ff476248574467e4b96a3
Sharod Duncan <sharod@me.com>
2021-03-24 09:17:45 -0700

    name cases exercise

---
+++ name_cases.py
@@ -0,0 +1,13 @@
+first_name = "sharod"
+last_name = "duncan"
+
+full_name = f"{first_name} {last_name}"
+
+#this line will print full_name in lower case
+print(full_name.lower())
+
+#this line will print full_name in all upper case
+print(full_name.upper())
+
+#this line will print full_name with capitalized first letters of the first_name and last_name
+print(full_name.title())
