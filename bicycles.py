9ddf5eb06ac8dedb917fd354f16021fac8da1565
Sharod Duncan <sharod@me.com>
2021-04-19 09:13:22 -0700

    bicycles excercise index list

--- bicycles.py
+++ bicycles.py
@@ -1,2 +1,9 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[0])
+print(bicycles[0].title()) 
+
+print(bicycles[1])
+print(bicycles[3])
+
+print(bicycles[-1]) # this is Python's syntax for accessing the last item in a list; you'll use a -1 to do this.
+
+
